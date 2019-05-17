484aff944de00434c4dcba7149f07b5620440ab9
hybridbad <hybridbad90@gmail.com>
2019-05-17 15:07:27 +0100

    Add test api route
    Test sends an object to the post route checks if successful
    Test passing
    
    Co-authored-by: Paul Martin <communty.hivemind@gmail.com>
    Co-authored-by: Riya Dattani <riya_dattani@hotmail.com>
    Co-authored-by: Wardah <wlt_93@hotmail.com>
    Co-authored-by: Cosmin <mirceacosmin@gmail.com>

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 const app = express();
-
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello from PlaceHolder. Deployed to Heroku from Travis. After a pull request.');
 });
+
+app.post('/api/data', (req, res) => {
+  data = req.body;
+  res.send(data);
+});
+
+
+
 
 app.server = app.listen(port, () => {
   console.log(`Running on port ${port}`);
--- tests/routeTests.js
+++ tests/routeTests.js
@@ -1,15 +1,15 @@
 const should = require('should');
 const sinon = require('sinon');
-const app = require('../app.js');
 const request = require('supertest');
+const app = require('../app.js');
 
 const agent = request.agent(app);
 
 describe('Testing "/" Route', () => {
   it('response status should be 200', (done) => {
     agent.get('/')
-      .end((err, results) => {
-        results.status.should.equal(200)
+      .end((err, res) => {
+        res.status.should.equal(200);
         done();
       });
   });
@@ -17,4 +17,24 @@ describe('Testing "/" Route', () => {
   after((done) => {
     app.server.close(done());
   });
-})
+});
+
+describe('Testing the api route', () => {
+  it('responds with a JASON X', (done) => {
+    request(app).post('/api/data')
+      .send({ data: { temperature: 9000 } })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      // eslint-disable-next-line consistent-return
+      .end((err, res) => {
+        if (err) return done(err);
+        res.status.should.equal(200);
+        done();
+      });
+  });
+
+  after((done) => {
+    app.server.close(done());
+  });
+});
